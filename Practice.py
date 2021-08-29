94ca8bb54e7cc1718b57f665a96bf0c08ea0ef50
Porteralon <63500021+Porteralon@users.noreply.github.com>
2021-08-29 13:02:35 +0300

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,18 @@
+MATH = 'Math'
+PHYSICS = 'Physics'
+SPORT = 'Sport'
+
+def main():
+    students = {}
+    id = 203834643
+    grade = 86
+    for i in range(10):
+        students.update({(id+i): {}})
+        students[id+i].update({MATH: (grade+i)})
+        students[id + i].update({PHYSICS: (grade - i)})
+        students[id + i].update({SPORT: (grade - (2*i))})
+    print(students)
+
+
+if __name__ == "__main__":
+    main()
